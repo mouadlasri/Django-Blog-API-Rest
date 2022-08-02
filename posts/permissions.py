16f335595bef94c7612a98d6a0122a771ff20531
Mouad <lasri.mouad98@gmail.com>
2022-08-02 10:52:41 +0900

    Custom permission, override BasePermission class

--- posts/permissions.py
+++ posts/permissions.py
@@ -4,10 +4,10 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
 
-    # override the has_object_permissio function of BasePermission class
+    # override the has_object_permission function of BasePermission class
     def has_object_permission(self, request, view, obj):
         # Read-only permissions are allowed for any request
-        #  If a request contains HTTP verbs included in SAFE_METHODS–a tuple containing GET, OPTIONS, and HEAD–then it is a read-only
+        # If a request contains HTTP verbs included in SAFE_METHODS–a tuple containing GET, OPTIONS, and HEAD–then it is a read-only
         # request and permission is granted.
         if request.method in permissions.SAFE_METHODS:
             return True
